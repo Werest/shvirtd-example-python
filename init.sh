8b17843aee01811735eed1ddc91f5768409b6127
Werest <33048498+Werest@users.noreply.github.com>
2025-06-29 17:46:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ cd $DIR/shvirtd-example-python
 docker build -t $IMAGE_NAME .
 
 # Авторизация в Container Registry через IAM-токен
-IAM_TOKEN=curl -s -H "Metadata-Flavor: Google" "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token" | jq -r '.access_token'
+IAM_TOKEN=$(curl -s -H "Metadata-Flavor: Google" "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token" | jq -r '.access_token')
 
 echo "$IAM_TOKEN" | docker login  --username iam  --password-stdin cr.yandex
 
